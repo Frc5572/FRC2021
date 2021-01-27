d03aa69e108aed68f74e7a705bf08519f80a12e9
klee111287 <54005889+klee111287@users.noreply.github.com>
2021-01-27 12:46:28 -0600

    Kevin Lee Ifdef and endif does not work?

--- src/main/include/Vision/VisionManager.hpp
+++ src/main/include/Vision/VisionManager.hpp
@@ -9,13 +9,13 @@
 
 class VisionManager {
     public:
-        double
-        disX = 0
-        , disY = 0
-        , tx = 0
-        , ty = 0
-        , ta = 0
-        , tv = 0;
+    double
+      disX = 0
+    , disY = 0
+    , tx = 0
+    , ty = 0
+    , ta = 0
+    , tv = 0;
 
     bool targetFound = false;
     void Update();
