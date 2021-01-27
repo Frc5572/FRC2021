7e3104e2d4316989e198c01965aad2bed42005ac
agrinmanriv0537 <75352922+agrinmanriv0537@users.noreply.github.com>
2021-01-27 10:04:45 -0600

    climbManagerLint

--- src/main/cpp/Movement/ClimbManager.cpp
+++ src/main/cpp/Movement/ClimbManager.cpp
@@ -6,47 +6,39 @@ ClimbManager::ClimbManager(
     frc::VictorSP& RightClimb,
     FRC5572Controller& Driver,
     frc::DoubleSolenoid& ClimbPistons
-){
+) {
     this->leftClimb = &LeftClimb;
     this->rightClimb = &RightClimb;
     this->driver = &Driver;
     this->climbPistons = &ClimbPistons;
-    //this->climbMotors = new frc::SpeedControllerGroup(LeftClimb, RightClimb);
-    //this->leftClimb->SetInverted(true);
-    //this->rightClimb->SetInverted(false);
-
+    // this->climbMotors = new frc::SpeedControllerGroup(LeftClimb, RightClimb);
+    // this->leftClimb->SetInverted(true);
+    // this->rightClimb->SetInverted(false);
 }
 
-void ClimbManager::ClimbPeriodic(){
+void ClimbManager::ClimbPeriodic() {
     ClimbManager::Spin();
     ClimbManager::UpAndDown();
 }
 
-void ClimbManager::UpAndDown(){
-    if(driver->LT() > 0){
+void ClimbManager::UpAndDown() {
+    if(driver->LT() > 0) {
         this->climbPistons->Set(frc::DoubleSolenoid::Value::kReverse);
-    }
-    else if(driver->RT() > 0){
+    }else if(driver->RT() > 0) {
        this->climbPistons->Set(frc::DoubleSolenoid::Value::kForward);
-    }
-    else{
+    }else {
         this->climbPistons->Set(frc::DoubleSolenoid::Value::kOff);
     }
-
 }
 
-
-void ClimbManager::Spin(){
-
-    if(driver->RB()){
+void ClimbManager::Spin() {
+    if(driver->RB()) {
         leftClimb->Set(.4);
         rightClimb->Set(.4);
-    }
-    else if(driver->LB()){
+    }else if(driver->LB()) {
         leftClimb->Set(-0.4);
         rightClimb->Set(-0.4);
-    }
-    else{
+    }else {
         leftClimb->Set(0.0);
         rightClimb->Set(0.0);
     }
