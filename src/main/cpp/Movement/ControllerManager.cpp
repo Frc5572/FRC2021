e565d2432208e6dbef3b75877a41989782f75f83
agrinmanriv0537 <75352922+agrinmanriv0537@users.noreply.github.com>
2021-01-26 20:58:37 -0600

    maybe done

--- src/main/cpp/Movement/ControllerManager.cpp
+++ src/main/cpp/Movement/ControllerManager.cpp
@@ -3,55 +3,53 @@
 #include <frc/GenericHID.h>
 
 FRC5572Controller::FRC5572Controller(int I) {
-    pad = new frc::XboxController (I); }
+    pad = new frc::XboxController (I);
+}
 
-FRC5572Controller::~FRC5572Controller()
-{	delete pad;						}
+FRC5572Controller::~FRC5572Controller(){
+    delete pad;
+}
 
-void FRC5572Controller::UpdateToggleA()
-{
- if (pad->GetRawButton(X_BUTTON))
-  {
-   if (!togglePressedA)
-	{
-        toggleA = !toggleA;
-        togglePressedA = true;
-    }
-  }
-  else
-  togglePressedA = false;
- }
+void FRC5572Controller::UpdateToggleA() {
+    if (pad->GetRawButton(X_BUTTON)){
+        if (!togglePressedA) {
+            toggleA = !toggleA;
+            togglePressedA = true;
+        }
+    }else
+    togglePressedA = false;
+}
 
 double FRC5572Controller::LT() {
- return pad->GetRawAxis(LEFT_z);
+    return pad->GetRawAxis(LEFT_z);
 }
 
 bool FRC5572Controller::LB() {
- return pad->GetRawButton(LEFT_BUTTON);
+    return pad->GetRawButton(LEFT_BUTTON);
 }
 
 double FRC5572Controller::RT() {
- return pad->GetRawAxis(RIGHT_z);
+    return pad->GetRawAxis(RIGHT_z);
 }
 
 bool FRC5572Controller::RB() {
- return pad->GetRawButton(RIGHT_BUTTON);
+    return pad->GetRawButton(RIGHT_BUTTON);
 }
 
 bool FRC5572Controller::X() {
- return pad->GetRawButton(X_BUTTON);
+    return pad->GetRawButton(X_BUTTON);
 }
 
 bool FRC5572Controller::Y() {
- return pad->GetRawButton(Y_BUTTON);
+    return pad->GetRawButton(Y_BUTTON);
 }
 
 bool FRC5572Controller::A() {
- return pad->GetRawButton(A_BUTTON);
+    return pad->GetRawButton(A_BUTTON);
 }
 
 bool FRC5572Controller::B() {
- return pad->GetRawButton(B_BUTTON);
+    return pad->GetRawButton(B_BUTTON);
 }
 
 std::pair<double, double> FRC5572Controller::L() {
