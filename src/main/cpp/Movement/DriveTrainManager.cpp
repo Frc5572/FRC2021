88a153d2da255eac3f19d82ec73cf332afb5a992
klee111287 <54005889+klee111287@users.noreply.github.com>
2021-01-27 11:17:15 -0600

    Kevin Lee Drive Train Manager Test 2

--- src/main/cpp/Movement/DriveTrainManager.cpp
+++ src/main/cpp/Movement/DriveTrainManager.cpp
@@ -69,8 +69,8 @@ DriveTrain::~DriveTrain(){
     delete ahrs;
 }
 
-//#define QUAD(x) (log2(x + 1) - 1)
- 
+// #define QUAD(x) (log2(x + 1) - 1)
+
 // void DriveTrain::Drive()
 // {
 //     if(this->Driver->L().second > .2 || this->Driver->L().second < -.2){
@@ -89,22 +89,22 @@ DriveTrain::~DriveTrain(){
 //     }
 // }
 
-void DriveTrain::Drive(){
+void DriveTrain::Drive() {
     DriveTrain::Aim();
-    if (this->Driver->L().second > .2 || this->Driver->L().second < -.2){
-        LeftMotors->Set(-1 * Driver->L().second * .7  );
+    if (this->Driver->L().second > .2 || this->Driver->L().second < -.2) {
+        LeftMotors->Set(-1 * Driver->L().second * .7);
     } else {
         LeftMotors->Set(0 + L);
     }
 
-    if (this->Driver->R().second > .2 ||  this->Driver->R().second < -.2){
+    if (this->Driver->R().second > .2 ||  this->Driver->R().second < -.2) {
         RightMotors->Set(Driver->R().second  * .7);
     } else {
         RightMotors->Set(0 + R);
     }
 }
 
-void DriveTrain::LowerAmps(){
+void DriveTrain::LowerAmps() {
     TopLeftMotor->SetSmartCurrentLimit(60);
     TopRightMotor->SetSmartCurrentLimit(60);
 
@@ -115,9 +115,8 @@ void DriveTrain::LowerAmps(){
     BottomRightMotor->SetSmartCurrentLimit(60);
 }
 
-void DriveTrain::Aim(){
-    if (Driver->X() ==  true)
-    {
+void DriveTrain::Aim() {
+    if (Driver->X() ==  true) {
         disX = LimeLight->disX;
         nt::NetworkTableInstance::GetDefault().GetTable("limelight")->PutNumber("camMode", 0);
         nt::NetworkTableInstance::GetDefault().GetTable("limelight")->PutNumber("ledMode", 3);
@@ -128,25 +127,20 @@ void DriveTrain::Aim(){
         L = 0; R = 0;
     }
 
-    if (fabs(disX) > 1 && Driver->X() == true)
-    {
-        if (disX > 10)
-        {
+    if (fabs(disX) > 1 && Driver->X() == true) {
+        if (disX > 10) {
             R = -.15;
             L = -.15;
         }
-        if (disX < 10)
-        {
+        if (disX < 10) {
             R = -disX/43;
             L = -disX/43;
         }
-        if (disX < -10)
-        {
+        if (disX < -10) {
             R = .15;
             L = .15;
         }
-        if (disX > -10)
-        {
+        if (disX > -10) {
             R = disX/43;
             L = disX/43;
         }
