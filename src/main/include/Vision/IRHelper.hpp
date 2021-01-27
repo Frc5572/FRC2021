2f6cf09b44f4d56360b380d02bf1bf6ff1955c2f
klee111287 <54005889+klee111287@users.noreply.github.com>
2021-01-27 15:57:00 -0600

    Kevin IRHelper test 2

--- src/main/include/Vision/IRHelper.hpp
+++ src/main/include/Vision/IRHelper.hpp
@@ -4,37 +4,37 @@
 #include <frc/AnalogInput.h>
 
 enum InfraredSensorTypev {
-	GP2Y0A710K0F, // 3 - 18 feet
-	GP2Y0A02YK0F, // 7 - 59 inches
-	OPB732WZ      // up to 3 inches
+    GP2Y0A710K0F, // 3 - 18 feet
+    GP2Y0A02YK0F, // 7 - 59 inches
+    OPB732WZ      // up to 3 inches
 };
 
 enum Color {
-	BLACK,
-	WHITE,
-	GRAY
+    BLACK,
+    WHITE,
+    GRAY
 };
 
 class Infrared {
 public:
-	Infrared(int irChannel, InfraredSensorType irType);
+    Infrared(int irChannel, InfraredSensorType irType);
 
-	Color checkColor();
-	double irRange();
-	double voltage();
-	int averageRaw();
+    Color checkColor();
+    double irRange();
+    double voltage();
+    int averageRaw();
 
 private:
-	static constexpr double COLOR_ERROR = 0.1;
-	static constexpr double WHITE_VALUE = 0.0;
-	static constexpr double BLACK_VALUE = 1.0;
-	static constexpr double GRAY_VALUE = 2.0;
+    static constexpr double COLOR_ERROR = 0.1;
+    static constexpr double WHITE_VALUE = 0.0;
+    static constexpr double BLACK_VALUE = 1.0;
+    static constexpr double GRAY_VALUE = 2.0;
 
-	double voltageScaling = 1.0;
-	double voltageIntercept = 1.0;
+    double voltageScaling = 1.0;
+    double voltageIntercept = 1.0;
 
     InfraredSensorType sensorFamily;
-	frc::AnalogInput* irSensor;
+    frc::AnalogInput* irSensor;
 };
 
 #endif  //IR_HELPER_HPP
