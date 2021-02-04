c57a2a2b166b3466fc56a0e3dde7be3265dde553
agrinmanriv0537 <75352922+agrinmanriv0537@users.noreply.github.com>
2021-02-04 17:10:58 -0600

    autowork

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -49,113 +49,47 @@ void Robot::AutonomousPeriodic() {
     // std::cout << ("\n Right Encoder \n");
     // std::cout << (BottomRightMotorEncoder->GetPosition());
 
-    if (ahrs.GetYaw() < 180 && ahrs.GetYaw() >= 0) {
+    //go forward 2 rotations
+    if (BottomLeftMotorEncoder->GetPosition() < 2) {
+        driveTrain.LeftMotors->Set(.3);
+        driveTrain.RightMotors->Set(.3);
+    //Turn left 90
+    } else if (abs(ahrs.GetYaw()) < 90 && ahrs.GetYaw() >= 0) {
         driveTrain.LeftMotors->Set(-.3);
         driveTrain.RightMotors->Set(.3);
-        //if (BottomLeftMotorEncoder->GetPosition() < 3) {
-        //    driveTrain.LeftMotors->Set(.3);
-        //    driveTrain.RightMotors->Set(.3);
-        //}
-    }
-    /*
-    else if (ahrs.GetYaw() > 0){
+    //go forward 2
+    } else if (BottomLeftMotorEncoder->GetPosition() > 2 && BottomLeftMotorEncoder->GetPosition() < 4) {
+        driveTrain.LeftMotors->Set(.3);
+        driveTrain.RightMotors->Set(.3);
+    //turn left 90
+    } else if (abs(ahrs.GetYaw()) < 180 && ahrs.GetYaw() >= 0) {
         driveTrain.LeftMotors->Set(-.3);
         driveTrain.RightMotors->Set(.3);
-    }
-    */
-    // else if (BottomLeftMotorEncoder->GetPosition() > 6 && BottomLeftMotorEncoder->GetPosition() < 9) {
-    //    driveTrain.LeftMotors->Set(.3);
-    //    driveTrain.RightMotors->Set(.3);
-    //}
-    else {
+    //go forward 2
+    } else if (BottomLeftMotorEncoder->GetPosition() > 4 && BottomLeftMotorEncoder->GetPosition() < 6) {
+        driveTrain.LeftMotors->Set(.3);
+        driveTrain.RightMotors->Set(.3);
+    //turn left 90
+    } else if (abs(ahrs.GetYaw()) > 90 && ahrs.GetYaw() <= 0) {
+        driveTrain.LeftMotors->Set(-.3);
+        driveTrain.RightMotors->Set(.3);
+    //go forward 2
+    } else if (BottomLeftMotorEncoder->GetPosition() > 6 && BottomLeftMotorEncoder->GetPosition() < 8) {
+        driveTrain.LeftMotors->Set(.3);
+        driveTrain.RightMotors->Set(.3);
+    //turn left 90-
+    } else if (abs(ahrs.GetYaw()) > 180 && ahrs.GetYaw() <= 0) {
+        driveTrain.LeftMotors->Set(-.3);
+        driveTrain.RightMotors->Set(.3);
+    } else {
         driveTrain.LeftMotors->Set(0);
         driveTrain.RightMotors->Set(0);
     }
 
-    //  while (m_timer.Get() < 3) {
-        // m_rightTopMotor.Set(-.1);
-        // m_rightMiddleMotor.Set(-.1);
-        // m_rightBottomMotor.Set(-.1);
-        // m_leftTopMotor.Set(.1);
-        // m_leftMiddleMotor.Set(.1);
-        // m_leftBottomMotor.Set(.1);
-        // driveTrain.RightMotors->Set(-.1);
-        // driveTrain.LeftMotors->Set(.1);
-    // }
-
-/*
-    while (m_timer.Get() < 3) {
-        //  shooter.AutoPID();
-        m_leftShooter.Set(.6);
-        m_rightShooter.Set(.6);
-        shooterHood.Set(frc::DoubleSolenoid::Value::kForward);
-        //  m_hopper.Set(.3);
-        continue;
-    }
-
-    while (m_timer.Get() > 3 && m_timer.Get() < 10) {
-        m_hopper.Set(.3);
-        m_leftShooter.Set(.6);
-        m_rightShooter.Set(.6);
-        shooterHood.Set(frc::DoubleSolenoid::Value::kForward);
-    }
-    while (m_timer.Get() > 10 && m_timer.Get() < 11) {
-        shooterHood.Set(frc::DoubleSolenoid::Value::kReverse);
-        m_leftShooter.Set(0);
-        m_rightShooter.Set(0);
-        m_hopper.Set(0.0);
-        continue;
-    }
-
-    while (m_timer.Get() > 11 && m_timer.Get() < 12) {
-        m_rightBottomMotor.Set(.3);
-        m_rightMiddleMotor.Set(.3);
-
-        m_leftBottomMotor.Set(-.3);
-        m_leftMiddleMotor.Set(-.3);
-        continue;
-    }
-*/
 
 
-
-
-//     //dripping springs auto fixed below
-//     while(m_timer.Get() < 2){
-//         m_leftShooter.Set(.7);
-//         m_rightShooter.Set(.7);
-//     }
-//     while(m_timer.Get() < 10 & m_timer.Get() > 2){
-//         shooterHood.Set(frc::DoubleSolenoid::Value::kForward);
-//         //intake.Set(frc::DoubleSolenoid::Value::kForward);
-//         m_leftShooter.Set(.7);
-//         m_rightShooter.Set(.7);
-//         m_hopper.Set(.3);
-//         continue;
-//     }
-//     while(m_timer.Get() > 10 && m_timer.Get() < 11){
-//         //intake.Set(frc::DoubleSolenoid::Value::kReverse);
-//         shooterHood.Set(frc::DoubleSolenoid::Value::kReverse);
-//         m_leftShooter.Set(0);
-//         m_rightShooter.Set(0);
-//         m_hopper.Set(0.0);
-//         continue;
-//     }
-
-//     while(m_timer.Get() > 11 && m_timer.Get() < 11.5){
-//         m_rightBottomMotor.Set(.3);
-//         m_rightMiddleMotor.Set(.3);
-
-//         m_leftBottomMotor.Set(-.3);
-//         m_leftMiddleMotor.Set(-.3);
-//         continue;
-//     }
-//         m_rightBottomMotor.Set(0);
-//         m_rightMiddleMotor.Set(0);
-
-//         m_leftBottomMotor.Set(0);
-//         m_leftMiddleMotor.Set(0);
 }
+
 
 void Robot::TeleopInit() {
     shooter.InitPID();
