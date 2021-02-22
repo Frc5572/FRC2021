196ebb63e51a3b730488ed7b5c14b7cf27ffea91
agrinmanriv0537 <75352922+agrinmanriv0537@users.noreply.github.com>
2021-02-22 17:30:19 -0600

    yftygtulc

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <tuple>
 
 #include <frc/Timer.h>
 #include <frc/smartdashboard/SmartDashboard.h>
@@ -10,9 +11,31 @@
 #include "Robot.h"
 #include "Movement/DriveTrainManager.hpp"
 
+std::tuple <double, double> E1;
+std::tuple <double, double> D2;
+std::tuple <double, double> B2;
+std::tuple <double, double> B4;
+std::tuple <double, double> D6;
+std::tuple <double, double> D7;
+std::tuple <double, double> B8;
+std::tuple <double, double> B9;
+std::tuple <double, double> D11;
+std::tuple <double, double> B11;
+//                              x, y
+std::tuple E1 = std::make_tuple(0, 0);
+std::tuple D2 = std::make_tuple(2.5, 2.5);
+std::tuple B2 = std::make_tuple(7.5, 2.5);
+std::tuple B4 = std::make_tuple(7.5, 7.5);
+std::tuple D6 = std::make_tuple(2.5, 12.5);
+std::tuple D7 = std::make_tuple(2.5, 15);
+std::tuple B8 = std::make_tuple(7.5, 17.5);
+std::tuple B9 = std::make_tuple(7.5, 20);
+std::tuple D11 = std::make_tuple(2.5, 25);
+std::tuple B11 = std::make_tuple(7.5, 25);
 
+double gridReturn(){
 
-
+}
 
 
 void Robot::RobotInit() {
@@ -32,105 +55,13 @@ void Robot::AutonomousInit()     {
     //  *BottomRightMotorEncoder};
 }
 void Robot::AutonomousPeriodic() {
-    //  while (m_timer.Get() < 15)
-    //  {
-    //  automovement->TestDrive();
-    //  continue;
-    //  }
 
-
-    while (m_timer.Get() < 3) {
-        //  shooter.AutoPID();
-        m_leftShooter.Set(.6);
-        m_rightShooter.Set(.6);
-        shooterHood.Set(frc::DoubleSolenoid::Value::kForward);
-        //  m_hopper.Set(.3);
-        continue;
-    }
-
-    while (m_timer.Get() > 3 && m_timer.Get() < 10) {
-        m_hopper.Set(.3);
-        m_leftShooter.Set(.6);
-        m_rightShooter.Set(.6);
-        shooterHood.Set(frc::DoubleSolenoid::Value::kForward);
-    }
-    while (m_timer.Get() > 10 && m_timer.Get() < 11) {
-        shooterHood.Set(frc::DoubleSolenoid::Value::kReverse);
-        m_leftShooter.Set(0);
-        m_rightShooter.Set(0);
-        m_hopper.Set(0.0);
-        continue;
-    }
-    while (m_timer.Get() > 11 && m_timer.Get() < 12) {
-        m_rightBottomMotor.Set(.3);
-        m_rightMiddleMotor.Set(.3);
-
-        m_leftBottomMotor.Set(-.3);
-        m_leftMiddleMotor.Set(-.3);
-        continue;
-    }
-    m_rightBottomMotor.Set(0);
-    m_rightMiddleMotor.Set(0);
-
-    m_leftBottomMotor.Set(0);
-    m_leftMiddleMotor.Set(0);
-
-
-
-
-//     //dripping springs auto fixed below
-//     while(m_timer.Get() < 2){
-//         m_leftShooter.Set(.7);
-//         m_rightShooter.Set(.7);
-//     }
-//     while(m_timer.Get() < 10 & m_timer.Get() > 2){
-//         shooterHood.Set(frc::DoubleSolenoid::Value::kForward);
-//         //intake.Set(frc::DoubleSolenoid::Value::kForward);
-//         m_leftShooter.Set(.7);
-//         m_rightShooter.Set(.7);
-//         m_hopper.Set(.3);
-//         continue;
-//     }
-//     while(m_timer.Get() > 10 && m_timer.Get() < 11){
-//         //intake.Set(frc::DoubleSolenoid::Value::kReverse);
-//         shooterHood.Set(frc::DoubleSolenoid::Value::kReverse);
-//         m_leftShooter.Set(0);
-//         m_rightShooter.Set(0);
-//         m_hopper.Set(0.0);
-//         continue;
-//     }
-
-//     while(m_timer.Get() > 11 && m_timer.Get() < 11.5){
-//         m_rightBottomMotor.Set(.3);
-//         m_rightMiddleMotor.Set(.3);
-
-//         m_leftBottomMotor.Set(-.3);
-//         m_leftMiddleMotor.Set(-.3);
-//         continue;
-//     }
-//         m_rightBottomMotor.Set(0);
-//         m_rightMiddleMotor.Set(0);
-
-//         m_leftBottomMotor.Set(0);
-//         m_leftMiddleMotor.Set(0);
 }
 
 void Robot::TeleopInit() {
-    shooter.InitPID();
 }
 
 void Robot::TeleopPeriodic() {
-     LimeLight.Update();
-
-    shooter.RunPID();
-
-    driveTrain.Drive();
-
-    shooter.Shots();
-
-    climber.ClimbPeriodic();
-
-    hopper.HopperPeriodic();
 }
 
 void Robot::TestInit() {
