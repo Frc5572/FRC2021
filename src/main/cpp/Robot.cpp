e72ae9397aadc3c1a7d0a323186a3282015794d1
agrinmanriv0537 <75352922+agrinmanriv0537@users.noreply.github.com>
2021-02-25 16:04:04 -0600

    tfu

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -15,7 +15,6 @@ double motorSpeed = 0.1;
 double gearRatio = 10;
 double wRotationFoot = 12 * gearRatio / 18.85;
 bool run_auto = true;
-int gearRatio = 10;
 double rotPFT = 12* gearRatio / 18.85;
 int robotPosL;
 int robotPosR;
