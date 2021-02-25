c2b54496f96d8bd518d1b95b3f91e25d7e5a3068
owenflatman <owenjflatman@gmail.com>
2021-02-25 17:29:05 -0600

    Progress

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -10,21 +10,46 @@
 #include "Robot.h"
 #include "Movement/DriveTrainManager.hpp"
 
+double motorSpeed = 0.1;
+double gearRatio = 10;
+double wRotationFoot = 12 * gearRatio / 18.85;
+int robotPosL1 = 0;
+int robotPosL2 = 0;
+int robotPosL3 = 0;
+int robotPosL4 = 0;
+int robotPosL5 = 0;
+int robotPosL6 = 0;
+int robotPosL7 = 0;
+int robotPosL8 = 0;
 
-
-
-
+bool run_auto = true;
 
 void Robot::RobotInit() {
+    driveTrain.RightMotors->SetInverted(true);
     m_timer.Start();
+    m_leftBottomMotor.RestoreFactoryDefaults();
+    m_leftMiddleMotor.RestoreFactoryDefaults();
+    m_leftTopMotor.RestoreFactoryDefaults();
+    m_rightBottomMotor.RestoreFactoryDefaults();
+    m_rightMiddleMotor.RestoreFactoryDefaults();
+    m_rightTopMotor.RestoreFactoryDefaults();
+    m_leftBottomMotor.SetIdleMode(rev::CANSparkMax::IdleMode::kCoast);
+    m_leftMiddleMotor.SetIdleMode(rev::CANSparkMax::IdleMode::kCoast);
+    m_leftTopMotor.SetIdleMode(rev::CANSparkMax::IdleMode::kCoast);
+    m_rightBottomMotor.SetIdleMode(rev::CANSparkMax::IdleMode::kCoast);
+    m_rightMiddleMotor.SetIdleMode(rev::CANSparkMax::IdleMode::kCoast);
+    m_rightTopMotor.SetIdleMode(rev::CANSparkMax::IdleMode::kCoast);
 }
 
 void Robot::RobotPeriodic() {
 }
-void Robot::AutonomousInit()     {
+void Robot::AutonomousInit()    {
     shooter.InitPID();
     m_timer.Reset();
     m_timer.Start();
+    ahrs.Reset();
+    BottomLeftMotorEncoder->SetPosition(0);
+    BottomRightMotorEncoder->SetPosition(0);
 
     //  automovement = new AutoMovement{*driveTrain.LeftMotors,
     //  *driveTrain.RightMotors,
@@ -32,105 +57,118 @@ void Robot::AutonomousInit()     {
     //  *BottomRightMotorEncoder};
 }
 void Robot::AutonomousPeriodic() {
-    //  while (m_timer.Get() < 15)
-    //  {
-    //  automovement->TestDrive();
-    //  continue;
-    //  }
-
-
-    while (m_timer.Get() < 3) {
-        //  shooter.AutoPID();
-        m_leftShooter.Set(.6);
-        m_rightShooter.Set(.6);
-        shooterHood.Set(frc::DoubleSolenoid::Value::kForward);
-        //  m_hopper.Set(.3);
-        continue;
+    BottomLeftMotorEncoder->SetPosition(0);
+    BottomRightMotorEncoder->SetPosition(0);
+    std::cout << ("Yaw:  \n");
+    std::cout << (ahrs.GetYaw());
+    // std::cout << ("\n Left Encoder \n");
+    // std::cout << (BottomLeftMotorEncoder->GetPosition());
+    std::cout << ("\n Right Encoder \n");
+    std::cout << (BottomRightMotorEncoder->GetPosition());
+    if (run_auto) {
+        while (BottomLeftMotorEncoder->GetPosition() <= wRotationFoot * 9.35) {
+            // forward 9.35 feet
+            driveTrain.LeftMotors->Set(motorSpeed);
+            driveTrain.RightMotors->Set(motorSpeed);
+        }
+        while (abs(ahrs.GetYaw()) < 90) {
+            // turn right 90
+            driveTrain.LeftMotors->Set(motorSpeed);
+            driveTrain.RightMotors->Set(-motorSpeed);
+            robotPosL1 = BottomLeftMotorEncoder->GetPosition();
+        }
+        while (BottomLeftMotorEncoder->GetPosition() <= robotPosL1 + wRotationFoot * 2.7) {
+            // forward 2.7 feet
+            driveTrain.LeftMotors->Set(motorSpeed);
+            driveTrain.RightMotors->Set(motorSpeed);
+        }
+        while (abs(ahrs.GetYaw()) < 180 && ahrs.GetYaw() < 0) {
+            // turn right 90
+            driveTrain.LeftMotors->Set(motorSpeed);
+            driveTrain.RightMotors->Set(-motorSpeed);
+            robotPosL1 = BottomLeftMotorEncoder->GetPosition();
+        }
+        while (BottomLeftMotorEncoder->GetPosition() <= robotPosL1 + wRotationFoot * 3.35) {
+            // forward 3.3 feet
+            driveTrain.LeftMotors->Set(motorSpeed);
+            driveTrain.RightMotors->Set(motorSpeed);
+        }
+        while (abs(ahrs.GetYaw()) > 90) {
+            // turn right 90
+            driveTrain.LeftMotors->Set(motorSpeed);
+            driveTrain.RightMotors->Set(-motorSpeed);
+            robotPosL1 = BottomLeftMotorEncoder->GetPosition();
+        }
+        while (BottomLeftMotorEncoder->GetPosition() <= robotPosL1 + wRotationFoot * 2.7) {
+            // forward 2.7 feet
+            driveTrain.LeftMotors->Set(motorSpeed);
+            driveTrain.RightMotors->Set(motorSpeed);
+        }
+        while (abs(ahrs.GetYaw()) > 11.77) {
+            // turn right 78.23
+            driveTrain.LeftMotors->Set(motorSpeed);
+            driveTrain.RightMotors->Set(-motorSpeed);
+            robotPosL1 = BottomLeftMotorEncoder->GetPosition();
+        }
+        while (BottomLeftMotorEncoder->GetPosition() <= robotPosL1 + wRotationFoot * 12.25) {
+            // forward 12.25 feet
+            driveTrain.LeftMotors->Set(motorSpeed);
+            driveTrain.RightMotors->Set(motorSpeed);
+        } 
+        while (abs(ahrs.GetYaw()) > 101.77) {
+            // turn left 90
+            driveTrain.LeftMotors->Set(-motorSpeed);
+            driveTrain.RightMotors->Set(motorSpeed);
+            robotPosL1 = BottomLeftMotorEncoder->GetPosition();
+        }
+        while (BottomLeftMotorEncoder->GetPosition() <= robotPosL1 + wRotationFoot * 3) {
+            // forward 3 feet
+            driveTrain.LeftMotors->Set(motorSpeed);
+            driveTrain.RightMotors->Set(motorSpeed);
+        } 
+        while (abs(ahrs.GetYaw()) < 11.77) {
+            // turn left 90
+            driveTrain.LeftMotors->Set(-motorSpeed);
+            driveTrain.RightMotors->Set(motorSpeed);
+            robotPosL1 = BottomLeftMotorEncoder->GetPosition();
+        }
+        while (BottomLeftMotorEncoder->GetPosition() <= robotPosL1 + wRotationFoot * 5.475) {
+            // forward 5.475 feet
+            driveTrain.LeftMotors->Set(motorSpeed);
+            driveTrain.RightMotors->Set(motorSpeed);
+        } 
+        while (abs(ahrs.GetYaw()) > 101.77) {
+            // turn left 90
+            driveTrain.LeftMotors->Set(-motorSpeed);
+            driveTrain.RightMotors->Set(motorSpeed);
+            robotPosL1 = BottomLeftMotorEncoder->GetPosition();
+        }
+        while (BottomLeftMotorEncoder->GetPosition() <= robotPosL1 + wRotationFoot * 3) {
+            // forward 3 feet
+            driveTrain.LeftMotors->Set(motorSpeed);
+            driveTrain.RightMotors->Set(motorSpeed);
+        } 
+        driveTrain.LeftMotors->Set(0);
+        driveTrain.RightMotors->Set(0);
+        run_auto = false;
     }
-
-    while (m_timer.Get() > 3 && m_timer.Get() < 10) {
-        m_hopper.Set(.3);
-        m_leftShooter.Set(.6);
-        m_rightShooter.Set(.6);
-        shooterHood.Set(frc::DoubleSolenoid::Value::kForward);
-    }
-    while (m_timer.Get() > 10 && m_timer.Get() < 11) {
-        shooterHood.Set(frc::DoubleSolenoid::Value::kReverse);
-        m_leftShooter.Set(0);
-        m_rightShooter.Set(0);
-        m_hopper.Set(0.0);
-        continue;
-    }
-    while (m_timer.Get() > 11 && m_timer.Get() < 12) {
-        m_rightBottomMotor.Set(.3);
-        m_rightMiddleMotor.Set(.3);
-
-        m_leftBottomMotor.Set(-.3);
-        m_leftMiddleMotor.Set(-.3);
-        continue;
-    }
-    m_rightBottomMotor.Set(0);
-    m_rightMiddleMotor.Set(0);
-
-    m_leftBottomMotor.Set(0);
-    m_leftMiddleMotor.Set(0);
-
-
-
-
-//     //dripping springs auto fixed below
-//     while(m_timer.Get() < 2){
-//         m_leftShooter.Set(.7);
-//         m_rightShooter.Set(.7);
-//     }
-//     while(m_timer.Get() < 10 & m_timer.Get() > 2){
-//         shooterHood.Set(frc::DoubleSolenoid::Value::kForward);
-//         //intake.Set(frc::DoubleSolenoid::Value::kForward);
-//         m_leftShooter.Set(.7);
-//         m_rightShooter.Set(.7);
-//         m_hopper.Set(.3);
-//         continue;
-//     }
-//     while(m_timer.Get() > 10 && m_timer.Get() < 11){
-//         //intake.Set(frc::DoubleSolenoid::Value::kReverse);
-//         shooterHood.Set(frc::DoubleSolenoid::Value::kReverse);
-//         m_leftShooter.Set(0);
-//         m_rightShooter.Set(0);
-//         m_hopper.Set(0.0);
-//         continue;
-//     }
-
-//     while(m_timer.Get() > 11 && m_timer.Get() < 11.5){
-//         m_rightBottomMotor.Set(.3);
-//         m_rightMiddleMotor.Set(.3);
-
-//         m_leftBottomMotor.Set(-.3);
-//         m_leftMiddleMotor.Set(-.3);
-//         continue;
-//     }
-//         m_rightBottomMotor.Set(0);
-//         m_rightMiddleMotor.Set(0);
-
-//         m_leftBottomMotor.Set(0);
-//         m_leftMiddleMotor.Set(0);
 }
-
 void Robot::TeleopInit() {
     shooter.InitPID();
 }
 
 void Robot::TeleopPeriodic() {
-     LimeLight.Update();
+    //  LimeLight.Update();
 
-    shooter.RunPID();
+    // shooter.RunPID();
 
     driveTrain.Drive();
 
-    shooter.Shots();
+    // shooter.Shots();
 
-    climber.ClimbPeriodic();
+    // climber.ClimbPeriodic();
 
-    hopper.HopperPeriodic();
+    // hopper.HopperPeriodic();
 }
 
 void Robot::TestInit() {
